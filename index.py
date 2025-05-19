921b6b073c4d99b0bc0e954f8c3172d68f03e79a
seqyu2 <seqyu@atl.tools>
2025-05-19 13:28:52 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,24 @@
+from thingz import accelerometer
 from thingz import led
+from thingz import sound
 
 def display_clear():
     print('[2J', end='')
 
-display_clear()
+def on_gesture_up(gesture):
+    display_clear()
+    print('')
+    // TODO: le jeu en lui même, faut arrêter de faire le con avec les easter eggs
+
+def on_gesture_shake(gesture):
+    sound.play_sample('rickroll')
+    print('vous me devez tous 10 balles (non)')
+
+accelerometer.on_gesture("up", on_gesture_up)
+accelerometer.on_gesture("shake", on_gesture_shake)
+
+print('Oriente la carte vers le haut.')
+led.set_colors(255, 255, 255)
 
 while True:
-    if False:
-        pass
-    else:
-        led.set_blue(255)
+    pass
